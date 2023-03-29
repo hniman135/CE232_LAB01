ddf42e36b15beda96adf2397bbdff62374011ebc
hniman135 <20521594@gm.uit.edu.vn>
2023-03-29 07:19:50 +0700

    Update blink.c

--- main/blink.c
+++ main/blink.c
@@ -11,12 +11,6 @@
 
 void app_main(void)
 {
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
     gpio_pad_select_gpio(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
